95bcbc453cd69faa67fa9742dbaa825b780e9e3d
ByronMG <byronmg212005@gmail.com>
2025-06-16 22:32:29 -0500

    landing Page Celerisys kbrones

---
+++ astro.config.mjs
@@ -0,0 +1,43 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
+
+export default defineConfig({
+  integrations: [tailwind(), react()],
+  output: "static",
+  build: {
+    inlineStylesheets: "auto",
+  },
+  server: {
+    host: false,
+    port: 4321,
+  },
+  vite: {
+    resolve: {
+      alias: {
+        "@": "/src",
+        "@components": "/src/components",
+      },
+    },
+    plugins: [
+      obfuscatorPlugin({
+        apply: "build",
+        exclude: [/node_modules/],
+        options: {
+          debugProtection: true,
+          compact: true,
+          controlFlowFlattening: true,
+          controlFlowFlatteningThreshold: 0.75,
+          deadCodeInjection: true,
+          deadCodeInjectionThreshold: 0.4,
+          stringArray: true,
+          stringArrayEncoding: ['base64'],
+          stringArrayThreshold: 0.75,
+          transformObjectKeys: true,
+        },
+      }),
+    ],
+  },
+});
